0258c55e76d69d3116588567e582e069752eaf80
brian120488 <brian.yang@csedge.org>
2020-06-20 12:35:53 -0400

    Added Home Menu

--- app/src/main/java/com/example/passwordmanager/MainMenuActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainMenuActivity.java
@@ -1,6 +1,8 @@
 package com.example.passwordmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,19 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToPasswords(View view) {
+        Intent intent = new Intent(this, PasswordsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToHome(View view) {
+
+    }
+
+    public void goToGenerator(View view) {
+        Intent intent = new Intent(this, PasswordsActivity.class);
+        startActivity(intent);
     }
 }
